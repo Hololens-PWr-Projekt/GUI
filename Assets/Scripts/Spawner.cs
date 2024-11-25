d3b285cd5fb4126333ffea1592d127991c34b258
serpenteno <fiacekk@gmail.com>
2024-11-25 11:42:21 +0100

    Stały punkt spawnu
    Instancja wybranego GameObject jest tworzona przed postacią gracza w odległości spawnDistance na płaszczyźnie XZ gracza.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     // The GameObject to spawn
+    [Tooltip("Obiekt, którego instancjê chcemy utworzyæ na scenie.")]
     public GameObject objToSpawn;
+
+    // Distance in front of Player where the GameObject will be spawned
+    [Tooltip("Odleg³oœæ w jakiej obiekt zostanie utworzony od gracza (przed graczem) [metry].")]
+    public float spawnDistance;
 
     // Reference to an existing instance to the GameObject
     private GameObject existingInstance;
@@ -18,12 +23,28 @@ public class Spawner : MonoBehaviour
             return;
         }
 
-        if (existingInstance != null)
+        // Main Camera on a scene, represents Player
+        Camera mainCamera = Camera.main;
+
+        if (mainCamera == null)
         {
-            Destroy(existingInstance); // If there is already an instance of the GameObject on a scene, destroy it.
+            Debug.LogWarning("No main camera on the scene has been found. Make sure there is a camera on the scene tagged as 'MainCamera'.");
+            return;
         }
 
-        existingInstance = Instantiate(objToSpawn, transform.position, transform.rotation);
+        Vector3 cameraForwardXZ = mainCamera.transform.forward;
+        cameraForwardXZ.y = 0; // The GameObject ought to be spawned only on Player's XZ plane (Y-axis independence)
+        cameraForwardXZ.Normalize();
+
+        Vector3 spawnPosition = mainCamera.transform.position + cameraForwardXZ * spawnDistance;
+        Quaternion spawnRotation = Quaternion.LookRotation(cameraForwardXZ); // The spawned GameObject will point upwards by default
+
+        if (existingInstance != null)
+        {
+            Destroy(existingInstance); // If there is already an instance of the GameObject on a scene, destroy it
+        }
+
+        existingInstance = Instantiate(objToSpawn, spawnPosition, spawnRotation);
 
     }
 
